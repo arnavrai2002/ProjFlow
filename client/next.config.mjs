e43649b5de1e53047bd08cc9d5e63097b740d2e4
Arnav <arnavrai5500@gmail.com>
2024-10-04 11:40:24 +0530

    Client

---
+++ client/next.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "pf-s3-images.s3.amazonaws.com",
+        port: "",
+        pathname: "/**",
+      }
+    ]
+  }
+};
+
+export default nextConfig;
